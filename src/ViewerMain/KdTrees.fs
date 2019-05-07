f53b309887f3243f9fde4d105b977eb6f5c03d99
walch <walch@vrvis.at>
2019-05-07 15:43:32 +0200

    fixed opc relative paths on load

--- src/ViewerMain/KdTrees.fs
+++ src/ViewerMain/KdTrees.fs
@@ -2,6 +2,7 @@ namespace Aardvark.VRVis.Opc
 
 open Aardvark.Geometry
 open Aardvark.Base
+open System.IO
 
 
 module KdTrees = 
@@ -23,3 +24,34 @@ module KdTrees =
   type Level0KdTree = 
       | LazyKdTree   of LazyKdTree
       | InCoreKdTree of InCoreKdTree
+      
+  let relativePath (remaining : int) (path : string)  =
+      path.Split(Path.DirectorySeparatorChar) 
+      |> List.ofArray 
+      |> List.rev 
+      |> List.take remaining 
+      |> List.rev 
+      |> Path.combine
+
+  let relativePath' (path : string) =
+    relativePath 3 path
+
+  let expandKdTreePaths basePath kd = 
+    kd 
+      |> HMap.map(fun _ k ->
+        match k with 
+          | Level0KdTree.LazyKdTree lkt -> 
+            let kdTreeSub   = lkt.kdtreePath    |> relativePath'
+            let triangleSub = lkt.objectSetPath |> relativePath'
+            let texturePath = lkt.texturePath   |> relativePath'
+            let coordinatePath = lkt.coordinatesPath |> relativePath'
+
+            LazyKdTree { 
+              lkt with 
+                  kdtreePath    = Path.Combine(basePath, kdTreeSub)
+                  objectSetPath = Path.Combine(basePath, triangleSub)
+                  texturePath   = Path.Combine(basePath, texturePath)
+                  coordinatesPath   = Path.Combine(basePath, coordinatePath)
+            }
+          | Level0KdTree.InCoreKdTree ik -> InCoreKdTree ik
+        )
