a59048a8912b77a5524a3470e865ed4c83a14b47
walch <walch@vrvis.at>
2019-08-07 12:52:05 +0200

    add AnnotationApp

---
+++ src/Rabbyte/Annotation/AnnotationApp.fs
@@ -0,0 +1,27 @@
+﻿namespace Rabbyte.Annotation
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.Base.Rendering
+open Aardvark.Application
+open Aardvark.SceneGraph
+
+open Aardvark.UI
+open Aardvark.UI.Primitives
+
+open OpcViewer.Base
+open FShade.Primitives
+
+open AnnotationModel
+
+module AnnotationApp =
+    open Rabbyte.Drawing
+
+    let update (model : AnnotationModel) (act : AnnotationAction) =
+        match act with
+        | AddDrawing brush -> 
+            { model with finishedDrawings = (model.finishedDrawings |> PList.prepend brush)}
+        | _ -> model
+
+    let view (model : MAnnotationModel) = 
+        model.finishedDrawings |> AList.map (fun x -> DrawingApp.view x) |> AList.toASet |> Sg.set
---
+++ src/Rabbyte/Annotation/AnnotationModel.fs
@@ -0,0 +1,105 @@
+﻿namespace Rabbyte.Annotation
+
+open System
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.UI
+
+open Rabbyte.Drawing
+
+[<DomainType>]
+type Annotation = {
+    version     : int
+
+    [<PrimaryKey; NonIncremental>]
+    key         : Guid
+
+    modelTrafo  : Trafo3d
+
+    //geometry    : Geometry   // Pro3D
+    //projection  : Projection // Pro3D
+    //semantic    : Semantic   // Pro3D
+
+    points      : plist<V3d>
+    segments    : plist<Segment>
+    style       : BrushStyle
+
+    //color       : ColorInput       // use Style...
+    //thickness   : NumericInput     // use Style...
+
+    //results     : Option<AnnotationResults'>      // Pro3D
+    //dnsResults  : Option<DipAndStrikeResults'>    // Pro3D
+
+    visible     : bool
+    //showDns     : bool  // Pro3d
+    text        : string
+    textsize    : float //NumericInput
+
+    surfaceName : string
+    //view        : CameraView // Pro3d
+
+    //semanticId   : SemanticId      // Pro3D
+    //semanticType : SemanticType    // Pro3D
+} with 
+    static member current = 1
+
+
+[<DomainType>]
+type AnnotationModel = {
+    finishedDrawings : plist<DrawingModel>
+}
+
+type AnnotationAction = 
+    | AddDrawing of DrawingModel
+    | RemoveDrawing of DrawingModel
+    | EditDrawing of DrawingModel
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module AnnotationModel = 
+    
+    let initAnnotation =
+        {
+            version     = Annotation.current
+            key         = Guid.NewGuid()
+            modelTrafo  = Trafo3d.Identity
+            //geometry    = geometry
+            //semantic    = Semantic.None
+            points      = plist.Empty
+            segments    = plist.Empty //[]
+            style       = DrawingModel.defaultStyle
+            //color       = color
+            //thickness   = thickness
+            //results     = None
+            //dnsResults  = None            
+            //projection  = projection
+            visible     = true
+            text        = ""
+            textsize    = 4.0
+            //showDns     = 
+            //    match geometry with 
+            //      | Geometry.DnS -> true 
+            //      | _ -> false 
+            surfaceName = ""
+            //view = FreeFlyController.initial.view
+        }
+
+    let convertDrawingToAnnotation (drawingModel:DrawingModel) = 
+        { 
+            initAnnotation with 
+                points = drawingModel.points
+                segments = drawingModel.segments
+                style = drawingModel.style
+        }
+
+    let convertAnnotationToDrawing (annotation:Annotation) =
+        { 
+            DrawingModel.inital with
+                style = annotation.style
+                points = annotation.points
+                segments = annotation.segments
+        }        
+
+    let initial =  
+        {
+            finishedDrawings = plist.Empty
+        }
